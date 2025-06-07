abf96de4d85ee3f8c287370b8b49620fe84536e8
thiago-cdb <fthiagocordeirodebrito@gmail.com>
2025-06-07 20:33:22 -0300

    Commit in 06/07/25

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
+var contactsRouter = require('./routes/contacts');
+var donateRouter = require('./routes/donate');
 
 var app = express();
 
@@ -13,14 +16,28 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Configurar express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'partials/layout'); // layout.ejs dentro de views/partials/
+
+// Middleware para setar a página ativa (para navbar)
+app.use((req, res, next) => {
+  const pathSegment = req.path.split('/')[1] || 'home';
+  res.locals.activePage = pathSegment;
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Rotas
+app.use('/', homeRouter);
+app.use('/about', aboutRouter);
+app.use('/contacts', contactsRouter);
+app.use('/donate', donateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
